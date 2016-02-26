12be0c8cb37de831556601cf8f24ee232668a0ba
username <email@email.com>
2016-02-26 14:18:57 +0200

    avoid roads topcoder

---
+++ AvoidRoads/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvoidRoads
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    public long numWays(int width, int height, String[] bad)
+    {
+      long numways = 0L;
+      int max = width + height;
+      int[][] map = new int[width + 1][];
+      for (int i = 0; i <= width; i++)
+      {
+        map[i] = Enumerable.Repeat(0, height + 1).ToArray();
+      }
+
+      for (int y = 0; y <= height; y++)
+      {
+        if (y == 0)
+        {
+          numways++;
+        }
+        for (int x = 0; x <= width; x++)
+        {
+
+          if (map[x][y] > max)
+          {
+            numways--;
+            continue;
+          }
+
+          //move forward
+          if (x < width)
+          {
+            if (x == 0 && y < height)
+            {
+              numways++;
+            }
+            map[x + 1][y]++;
+          }
+
+          //move up
+          if (y < height)
+          {
+            if (x > 0 && x < width)
+            {
+              numways++;
+              if (y > 0) //&& y<height
+              {
+                numways++;
+              }
+            }
+            map[x][y + 1]++;
+          }
+        }
+
+      }
+
+      return numways;
+    }
+
+    public long numWays2(int width, int height, String[] bad)
+    {
+      int max = width + height;
+      long[][] map = new long[width + 1][];
+      for (int i = 0; i <= width; i++)
+      {
+        map[i] = Enumerable.Repeat(0L, height + 1).ToArray();
+      }
+
+      int[][] badRoads = new int[bad.Length][];
+
+      for (int i = 0; i < bad.Length; i++)
+      {
+        string[] coords = bad[i].Split(null);
+        int x1 = int.Parse(coords[0]);
+        int y1 = int.Parse(coords[1]);
+        int x2 = int.Parse(coords[2]);
+        int y2 = int.Parse(coords[3]);
+
+        sort(ref x1, ref y1, ref x2, ref y2);
+
+        badRoads[i] = new int[] { x1, y1, x2, y2 };
+
+      }
+
+      map[0][0] = 1;
+      for (int y = 0; y <= height; y++)
+      {
+
+        for (int x = 0; x <= width; x++)
+        {
+          if (x == 0 && y == 0)
+          {
+            continue;
+          }
+
+          map[x][y] += x > 0 && isOpen(x - 1, y, x, y, badRoads) ? map[x - 1][y] : 0;
+          map[x][y] += y > 0 && isOpen(x, y - 1, x, y, badRoads) ? map[x][y - 1] : 0;
+        }
+
+      }
+
+      return map[width][height];
+    }
+
+    private bool isOpen(int x1, int y1, int x2, int y2, int[][] badRoads)
+    {
+      return badRoads==null || badRoads.Length==0 || 
+        (badRoads.Where(a=>a[0]== x1 && a[1]== y1 && a[2]== x2 && a[3]== y2).Count()==0 &&
+        badRoads.Where(a => a[0] == x2 && a[1] == y2 && a[2] == x1 && a[3] == y1).Count() == 0);
+    }
+
+    private void sort(ref int x1, ref int y1, ref int x2, ref int y2)
+    {
+      if (x2 < x1 || y2 < y1)
+      {
+        swop(ref x1, ref x2);
+        swop(ref y1, ref y2);
+      }
+    }
+
+    private void swop(ref int a, ref int b)
+    {
+      int tmp = a;
+      a = b;
+      b = tmp;
+    }
+  }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AvoidRoads;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    private readonly int W = 0;
+    private readonly int H = 0;
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+      int[][] allWH = new int[][] {
+
+         new int[] { 1,1 },
+         new int[] { 1,1 },
+          new int[] { 1,1 },
+          new int[] { 1,1 },
+          new int[] { 2,2 },
+          new int[] { 2,2 },
+          new int[] { 2,2 },
+          new int[] { 2,2 },
+          new int[] { 3,3 },
+          new int[] { 3,3 },
+          new int[] { 3,3 },
+        new int[] { 6, 6 },
+        new int[] { 35, 31 }
+      };
+
+
+      string[][] allBad = new string[][] {
+
+        new string[] { },
+            new string[] {"0 0 0 1" },
+            new string[] {"0 0 1 0" },
+            new string[] { "0 0 0 1", "0 0 1 0" },
+        new string[] { },
+           new string[] {"1 1 1 2" },
+           new string[] {"1 1 1 2", "1 1 2 1" },
+                new string[] { "0 0 1 0","1 1 1 2", "1 1 2 1" },
+            new string[] { },
+             new string[] {"1 1 1 2" },
+             new string[] {"0 0 1 0" },
+        new string[] { "0 0 0 1", "6 6 5 6" },
+        new string[] { }
+      };
+
+      long[] allNums = new long[] { 2L, 1L, 1L, 0L, 6L, 4L, 2L, 0L, 20L, 14L, 10L, 252L, 6406484391866534976L };
+
+      Program p = new Program();
+
+      int length = allNums.Length;
+      for (int i = 0; i < length - 1; i++)
+      {
+        int width = allWH[i][W];
+        int height = allWH[i][H];
+        string[] bad = allBad[i];
+
+        long actual = p.numWays2(width, height, bad);
+
+        long expected = allNums[i];
+
+        Assert.AreEqual(expected, actual);
+      }
+    }
+  }
+}
